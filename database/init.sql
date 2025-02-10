e3f844af292f8b33445527cae4b692b4dd2c1685
adityagupta19 <adityagupta11219@gmail.com>
2025-02-11 00:08:21 +0530

    Added the init.sql for initial migrations

---
+++ database/init.sql
@@ -0,0 +1,18 @@
+\c battleship;
+/*
+To run this script delete the image and volume of the database container
+*/
+
+
+
+/*
+This table stores user data with:
+- id: Unique identifier
+- username: Unique player name
+- rating: Elo-style rating (default 1000)
+*/
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    rating INT DEFAULT 1000
+);
