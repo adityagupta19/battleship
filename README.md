8372db54253383a3033d0d6d164949161931fa72
adityagupta19 <adityagupta11219@gmail.com>
2025-02-16 23:52:35 +0530

    Added design v1 image

---
+++ README.md
@@ -0,0 +1,1 @@
+![DesignV1](battleship_design_v1.svg)
--- matchmaking/internal/matchmaking.go
+++ matchmaking/internal/matchmaking.go
@@ -1,1 +1,19 @@
 package internal
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+
+)
+
+func pushUserToQueue(userId uint) (error){
+
+}
+
+
+func createGamesWorker() {
+
+}
+
+func expireRequestsWorker() {
+
+}
